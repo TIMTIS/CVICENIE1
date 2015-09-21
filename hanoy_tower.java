535d084a13b141345cce326fb497c36d3e5b9793
jurasek7 <jurasek7@me>
2015-09-21 18:42:58 +0200

    komentare a zmeneny pocet diskov

--- hanoy_tower.java
+++ hanoy_tower.java
@@ -1,10 +1,11 @@
  
-
+//deklaracia triedy , procedura main spusti program, ktory vyriesi zadanu hanojsku vezu
 public class MainClass {
   public static void main(String[] args) {
-    int nDisks = 3;
+    int nDisks = 4;
     doTowers(nDisks, 'A', 'B', 'C');
   }
+  
 
   public static void doTowers(int topN, char from, char inter, char to) {
     if (topN == 1){
